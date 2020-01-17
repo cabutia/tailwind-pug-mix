11ac49e0d0478c331c7cd64b123e2ace19a30950
Leandro Gomez <leandrogrotnig11@gmail.com>
2020-01-17 13:02:25 -0300

    Working

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
+let LiveReloadPlugin = require('webpack-livereload-plugin')
+
 let mix = require('laravel-mix')
 let tailwind = require('tailwindcss')
+mix.pug = require('laravel-mix-pug')
 
 mix
 	.js('src/resources/js/app.js', 'dist/')
@@ -8,3 +11,12 @@ mix
 		processCssUrls: false,
 		postCss: [tailwind('./tailwind.config.js')]
 	})
+	.pug('src/pages/**/*.pug', '../../dist/')
+
+mix.webpackConfig({
+	devServer: {
+		host: "localhost",
+		port: "5050",
+		contentBase: path.join(__dirname, 'dist')
+	}
+})
